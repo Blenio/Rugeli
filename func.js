200893dbba08d802ff6b411e8801eb2bf8e683ed
Blenio <44197127+Blenio@users.noreply.github.com>
2018-10-16 14:47:36 +0200

    Update func.js

--- func.js
+++ func.js
@@ -1,10 +1,12 @@
-function myFunction() {
+function checkButton() {
   var x = document.getElementById("02Text").value;
   document.getElementById("result").innerHTML = x;
 }
 
-var dateToday = new Date().getDate(); 
-if (dateToday == 16) {
-    document.getElementById("result").innerHTML = "Good day 16.10";
+function checkDate() {
+  var dateToday = new Date().getDate(); 
+  if (dateToday == 16) {
+      document.getElementById("result").innerHTML = "Good day 16.10";
+  }
 }
-document.getElementById("result").innerHTML = "Good day ";
+
