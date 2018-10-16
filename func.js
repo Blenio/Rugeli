a817b9c9a86e9314c48a03bce60d6f5338551035
Blenio <44197127+Blenio@users.noreply.github.com>
2018-10-16 15:08:33 +0200

    Update func.js

--- func.js
+++ func.js
@@ -1,14 +1,23 @@
 function checkButton() {
-  var x = document.getElementById("02Text").value;
-  document.getElementById("result").innerHTML = x;
+  var x = document.getElementById("loesung").value;
+  document.getElementById("hint").innerHTML = x;
 }
 
 function checkDate() {
-  var dateToday = new Date().getDate(); 
-  if (dateToday == 16) {
-    document.getElementById("result2").innerHTML = dateToday;
-  } else {
-    document.getElementById("result2").innerHTML = "Kein Rätseltag " + dateToday;
-  }
+  var myDay = new Date().getDate(); 
+  
+  switch(myDay) {
+    case 02:
+        document.getElementById("puzzle").innerHTML = "Text Puzzle 02";
+        break;
+    case 04:
+        document.getElementById("puzzle").innerHTML = "Text Puzzle 02";
+        break;
+    case 16:
+        document.getElementById("puzzle").innerHTML = "Text Puzzle 02";
+        break;
+    default:
+        text = "Kein Rätseltag..." + myDay;
+    }
 }
 
