8e5aade75adc31c9b3fdbc11554beb189be1b1aa
Blenio <44197127+Blenio@users.noreply.github.com>
2018-10-16 14:00:39 +0200

    Update func.js

--- func.js
+++ func.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#interaktiv').addEventListener('click', klickverarbeitung);
  
   function klickverarbeitung () {
-    document.querySelector('output').innerText + ' Huhu, das ist von Javascript eingefügter Text. \n';
+    document.querySelector('output').innerText = ' Geklickt \n';
   }
 	  
 });
