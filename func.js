dcc9ff30720a50cde246ab5d26f4c362d5fdd776
Blenio <44197127+Blenio@users.noreply.github.com>
2018-10-18 13:43:50 +0200

    Update func.js

--- func.js
+++ func.js
@@ -38,11 +38,11 @@ function checkDate() {
    }
   
   switch(myDay) {
-    case 2:
-      thePuzzle = "Rätsel 2";
-      theHint = "Hint 2";
-      theSolution = "a";
-      thePresent = "Das Geschenk ist 2";
+    case 18:
+      thePuzzle = "Rätsel 02:<br>.Zahlen drei suchen Wir<br>---<br>Tag 5.: Morgen wurde es & Abend wurde es...";
+      theHint = "Aus einem sehr erfolgreiches Buch";
+      theSolution = "123";
+      thePresent = "Das Geschenk ist unter der Türmatte";
       break;
     case 4:
       thePuzzle = "Rätsel 4";
@@ -50,14 +50,68 @@ function checkDate() {
       theSolution = "b";
       thePresent = "Das Geschenk ist 4";
       break;
-    case 17:
+    case 6:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;
+    case 8:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;
+    case 10:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;
+    case 12:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;
+    case 14:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;    
+    case 16:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;
+    case 18:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;
+    case 20:
+      thePuzzle = "Rätsel 4";
+      theHint = "Hint 4";
+      theSolution = "b";
+      thePresent = "Das Geschenk ist 4";
+      break;      
+    case 22:
       thePuzzle = "Rätsel 17"; 
       theHint = "Hint 17";
       theSolution = "c";
       thePresent = "Das Geschenk ist 17";
       break;
+    case 24:
+      thePuzzle = "Rätsel 17"; 
+      theHint = "Hint 17";
+      theSolution = "c";
+      thePresent = "Das Geschenk ist 17";
+      break;      
     default:
-      thePuzzle = "Leider kein Rätseltag heute...";
+      thePuzzle = "Leider kein Rätseltag heute...<br>";
       theHint = "";
       theSolution = "";
       thePresent = "";
