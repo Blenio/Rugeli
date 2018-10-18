b595383e82b42b3c05120081af15da08971fb0d8
Blenio <44197127+Blenio@users.noreply.github.com>
2018-10-18 16:54:31 +0200

    Update func.js

--- func.js
+++ func.js
@@ -39,76 +39,76 @@ function checkDate() {
   
   switch(myDay) {
     case 2:
-      thePuzzle = "<b>Rätsel vom 2. Dezember:<b><br>.Zahlen drei suchen Wir<br>---<br>Tag 5.: Morgen wurde es & Abend wurde es...";
+      thePuzzle = ".Zahlen drei suchen Wir<br>---<br>Tag 5.: Morgen wurde es & Abend wurde es...";
       theHint = "Aus einem sehr, sehr erfolgreichen Buch";
       theSolution = "123";
-      thePresent = "Das Geschenk ist unter der Türmatte";
+      thePresent = "Das Geschenk ist unter einer Türmatte";
       break;
-    case 18:
-      thePuzzle = "<b>Rätsel vom 4. Dezember:<b><br>x x _ x x x _ _ _ ist die weibliche Hauptrolle in welchem (dir bekannten) Film?<br>---<br>(28 657,  46 368,  75 025, 121 393) ";
+    case 4:
+      thePuzzle = "x x _ x x x _ _ _ ist die weibliche Hauptrolle in welchem (dir bekannten) Film?<br>---<br>(28 657,  46 368,  75 025, 121 393) ";
       theHint = "Berühmter mathematischer Satz";
       theSolution = "shrek";
       thePresent = "Das Geschenk ist oben auf unserem Schlafzimmerschrank";
       break;
     case 6:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "chay' ngo' ali?";
+      theHint = "Sprache aus StarTrek";
+      theSolution = "74";
+      thePresent = "Das Geschenk ist oben auf dem Kühlschrank";
       break;
     case 8:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "2 & 4. Buchstabe des Vornames des jüngeren Bruders des Ex von der Schwester von Mary Jo White-McCurdy";
+      theHint = "Jamie White";
+      theSolution = "ak";
+      thePresent = "Das Geschenk ist in meiner Sockenschublade";
       break;
     case 10:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "Wer liebt: 48, 3C und 32?";
+      theHint = "Hex-Code";
+      theSolution = "marianne";
+      thePresent = "Das Geschenk ist in einem Regenstiefel";
       break;
     case 12:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "GTX de Luxe GS<br>Frage: (5.) (-) (1. & 9.)";
+      theHint = "Hägrid";
+      theSolution = "evh";
+      thePresent = "Das Geschenk ist im Altpapier";
       break;
     case 14:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "Wir suchen die Einwohnerzahl (Stand 2017) eines Landes, welches abstrakte Herzen liebt";
+      theHint = "3 grüne Rauten";
+      theSolution = "109897";
+      thePresent = "Das Geschenk ist bei meinem Roller";
       break;    
     case 16:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "Translator sind wertvoll für Tipps";
+      theHint = "Bekannte Übersetzerinnen noch mehr";
+      theSolution = "tine";
+      thePresent = "Du weisst nun also wen fragen :-)";
       break;
     case 18:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "Weisse Reklame-Tafel in NYC";
+      theHint = "Poster";
+      theSolution = "westsidestory";
+      thePresent = "Das Geschenk ist im Schrimständer";
       break;
     case 20:
-      thePuzzle = "Rätsel 4";
-      theHint = "Hint 4";
-      theSolution = "b";
-      thePresent = "Das Geschenk ist 4";
+      thePuzzle = "Wir suchen eine 4-stellige Zahl. Was wäre, wenn uns jemand eine Postkarte vor 134.092.800 Sekunden schreiben wollte?";
+      theHint = "(A)";
+      theSolution = "6532";
+      thePresent = "Das Geschenk ist unter Deiner Schreibtischmatte";
       break;      
     case 22:
-      thePuzzle = "Rätsel 17"; 
-      theHint = "Hint 17";
-      theSolution = "c";
-      thePresent = "Das Geschenk ist 17";
+      thePuzzle = "Was:Calciumcarbonat<br>Wo: Carpenters / Bitte Wuschu"; 
+      theHint = "Wikipedia, 1. Modifikation der Natur / Song";
+      theSolution = "8a7";
+      thePresent = "Bringe ich nach Hause, wenn Du mir *Der Adler ist gelandet* per SMS schreibst"";
       break;
     case 24:
-      thePuzzle = "Rätsel 17"; 
-      theHint = "Hint 17";
-      theSolution = "c";
-      thePresent = "Das Geschenk ist 17";
+      thePuzzle = "Doppeltes Glück zwischen einem kurzen Monat"; 
+      theHint = "13";
+      theSolution = "lilian";
+      thePresent = "Das Geschenk ist unter einem Kopfkissen";
       break;      
     default:
       thePuzzle = "Leider kein Rätseltag heute...<br>";
